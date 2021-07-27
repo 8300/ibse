59a7082a9e0c99ab0fba01dbdbb5e6ffbad9c7df
ts <ts@debian.ts>
2021-07-27 14:26:06 +0800

    u

---
+++ gateway/LT5135A/io.c
@@ -0,0 +1,13 @@
+﻿OUT0:
+echo 1 > /sys/class/gpio/gpio115/value   /*开 */
+echo 0 > /sys/class/gpio/gpio115/value   /*闭 */
+
+OUT1:
+echo 1 > /sys/class/gpio/gpio114/value   /*开 */
+echo 0 > /sys/class/gpio/gpio114/value   /*闭 */
+
+IN0:
+cat /sys/class/gpio/gpio117/value
+
+IN1:
+cat /sys/class/gpio/gpio20/value
